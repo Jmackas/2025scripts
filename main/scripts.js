3db2fc59a63315be78c226325d7a0b761449c9aa
James Mackay <jamesrossmackay@gmail.com>
2025-04-21 11:07:44 +0800

    Update scripts.js

--- main/scripts.js
+++ main/scripts.js
@@ -1,8 +1,14 @@
 /*
 
+#### 
 This file URL:
 https://jmackas.github.io/2025scripts/main/scripts.js
 
+#### 
+To inject into another website:
+document.head.appendChild(Object.assign(document.createElement('script'), {src: 'https://jmackas.github.io/2025scripts/main/scripts.js'}));
+
+#### 
 Add the below to add some code from a different file:
 document.head.appendChild(Object.assign(document.createElement('script'), {src: 'https://google.com'}));
 
