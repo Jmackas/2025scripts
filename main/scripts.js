c2262e7e561b8b0426eb9ad95af802d9dd36ba1a
James Mackay <jamesrossmackay@gmail.com>
2025-04-21 10:59:10 +0800

    Update scripts.js

--- main/scripts.js
+++ main/scripts.js
@@ -1,10 +1,14 @@
 /*
 
+This file URL:
+https://jmackas.github.io/2025scripts/main/scripts.js
+
 Add the below to add some code from a different file:
 document.head.appendChild(Object.assign(document.createElement('script'), {src: 'https://google.com'}));
 
 */
 
-console.log("helloooo");
-
-// yoo
+//
+// ClickUp
+//
+document.head.appendChild(Object.assign(document.createElement('script'), {src: '/clickup/windowed-mode.js'}));
