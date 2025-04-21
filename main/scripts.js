6b30aaa472b7e3c20135dc29ac32c0878e9e807a
James Mackay <jamesrossmackay@gmail.com>
2025-04-21 11:05:56 +0800

    Update scripts.js

--- main/scripts.js
+++ main/scripts.js
@@ -9,6 +9,21 @@ document.head.appendChild(Object.assign(document.createElement('script'), {src: 
 */
 
 //
+// All CSS
+//
+// Create a new <link> element
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'https://jmackas.github.io/2025scripts/main/styles.css'; // Replace with your remote CSS file URL
+
+
+
+// Append the <link> element to the <head> of the document
+document.head.appendChild(link);
+
+
+//
 // ClickUp
 //
 document.head.appendChild(Object.assign(document.createElement('script'), {src: 'https://jmackas.github.io/2025scripts/main/clickup/windowed-mode.js'}));
