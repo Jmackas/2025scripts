4c805afbe26d0b060e3b1fe339cd6d5bd01dc995
James Mackay <jamesrossmackay@gmail.com>
2025-04-22 14:17:36 +0800

    Delete main/misc directory

--- main/misc/disable-ctrl-s.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('keydown', function(e) {
-  if (e.ctrlKey && e.key === 's') {
-    e.preventDefault();
-    // Add your custom save logic here
-    console.log('Ctrl+S pressed, custom save triggered');
-  }
-});
